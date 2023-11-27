69337ca729150353d5d1233e75021ecee1e88e4f
Greg Ells <gregells@gmail.com>
2023-11-27 14:18:16 +1300

    Server.js and package.json updated.

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a>``
       </div>
       <h1>Vite + React</h1>
       <div className="card">
